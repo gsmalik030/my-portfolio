ded7c7a45abf945ae1ac418475bb80a852b39f46
Malik Ghulam Subbhani <gsmalik030@gmail.com>
2023-06-27 15:16:19 +0200

    Added all the necessory components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Round from './components/roundDesigns/Round';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="w-full h-screen font-bodyfont text-textColor bg-black relative">
+      <section className="max-w-screen-2xl h-full mx-auto flex justify-center items-center">
+        <Home />
+      </section>
+      <Round />
+    </section>
   );
 }
 
